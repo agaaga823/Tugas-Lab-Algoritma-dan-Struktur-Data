7ed6754a2de5d778ba463a7a569de2ff2962d89f
agaaga823 <113017632+agaaga823@users.noreply.github.com>
2022-11-23 14:48:28 +0700

    Add files via upload

---
+++ Tugas 10 no 2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void tambah(int c,int d);
+  
+int main(){
+  int a = 4, b = 6;
+  cout << "Nilai Sebelum Pemanggilan Fungsi";
+  cout << "\na = "<< a << " b = " << b;
+  tambah(a,b);
+  getchar();
+  }
+
+ void tambah(int c,int d){
+   c+=7;
+   d+=5;
+   cout << endl;
+   cout << "\nNilai di Akhir Fungsi Tambah()";
+   cout << "\nc = " << c << " d = " << d;
+   cout << endl;
+   cout << "\nNilai Setelah Pemanggilan Fungsi";
+   cout << "\na = " << c << " b = " << d;
+    }
